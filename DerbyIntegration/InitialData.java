8638d009f5008def31e1cb8415a7e7076311fce4
ndow1021 <ndow@ycp.edu>
2023-03-29 13:46:19 -0400

    InitialData.java changed to fit our csv files

--- DerbyIntegration/InitialData.java
+++ DerbyIntegration/InitialData.java
@@ -9,51 +9,51 @@ import edu.ycp.cs320.booksdb.model.Author;
 import edu.ycp.cs320.booksdb.model.Book;
 
 public class InitialData {
-	public static List<Author> getAuthors() throws IOException {
-		List<Author> authorList = new ArrayList<Author>();
-		edu.ycp.cs320.booksdb.persist.ReadCSV readAuthors = new edu.ycp.cs320.booksdb.persist.ReadCSV("authors.csv");
+	public static List<Users> getAuthors() throws IOException {
+		List<Users> userList = new ArrayList<Users>();
+		edu.ycp.cs320.booksdb.persist.ReadCSV readUsers = new edu.ycp.cs320.booksdb.persist.ReadCSV("authors.csv");
 		try {
 			// auto-generated primary key for authors table
-			Integer authorId = 1;
+			Integer userId = 1;
 			while (true) {
 				List<String> tuple = readAuthors.next();
 				if (tuple == null) {
 					break;
 				}
 				Iterator<String> i = tuple.iterator();
-				Author author = new Author();
-				author.setAuthorId(authorId++);				
-				author.setLastname(i.next());
-				author.setFirstname(i.next());
-				authorList.add(author);
+				User user = new User();
+				user.setUserId(userId++);
+				user.setLastname(i.next());
+				user.setFirstname(i.next());
+				userList.add(user);
 			}
-			return authorList;
+			return userList;
 		} finally {
-			readAuthors.close();
+			readUsers.close();
 		}
 	}
 	
-	public static <ReadCSV> List<Book> getBooks() throws IOException {
-		List<Book> bookList = new ArrayList<Book>();
+	public static <ReadCSV> List<userData> getBooks() throws IOException {
+		List<userData> dataList = new ArrayList<userData>();
 		ReadCSV readBooks = new ReadCSV("books.csv");
 		try {
 			// auto-generated primary key for books table
-			Integer bookId = 1;
+			Integer dataId = 1;
 			while (true) {
-				List<String> tuple = readBooks.next();
+				List<String> tuple = readData.next();
 				if (tuple == null) {
 					break;
 				}
 				Iterator<String> i = tuple.iterator();
-				Book book = new Book();
-				book.setBookId(bookId++);
-				book.setAuthorId(Integer.parseInt(i.next()));
-				book.setTitle(i.next());
-				book.setIsbn(i.next());
-				book.setPublished(Integer.parseInt(i.next()));
-				bookList.add(book);
+				userData data = new userData();
+				data.setBookId(dataId++);
+				data.setAuthorId(Integer.parseInt(i.next()));
+				data.setTitle(i.next());
+				data.setIsbn(i.next());
+				data.setPublished(Integer.parseInt(i.next()));
+				dataList.add(data);
 			}
-			return bookList;
+			return dataList;
 		} finally {
 			readBooks.close();
 		}
