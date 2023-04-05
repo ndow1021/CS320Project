42b4f464951d195532ba1c0cdcf79bff1e14aab4
ndow1021 <ndow@ycp.edu>
2023-04-05 14:24:40 -0400

    added QuizController

---
+++ QuizController.java
@@ -0,0 +1,21 @@
+public class QuizController {
+
+    private int score;
+
+    public int getScore()
+    {
+        return score;
+    }
+    public void setScore(int number)
+    {
+        score = number;
+    }
+    public void addToScore(int number)
+    {
+        score = score + number;
+    }
+
+
+
+
+}
