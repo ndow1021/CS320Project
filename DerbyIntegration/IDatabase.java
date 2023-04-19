58e78b5f281e9a7f9ad67fb12d910b6b24ccb73d
ndow1021 <ndow@ycp.edu>
2023-04-19 13:48:06 -0400

    aaaaa

--- DerbyIntegration/IDatabase.java
+++ DerbyIntegration/IDatabase.java
@@ -7,5 +7,5 @@ import edu.ycp.cs320.booksdb.model.Book;
 import edu.ycp.cs320.booksdb.model.Pair;
 
 public interface IDatabase {
-	public List<Pair<Author, Book>> findAuthorAndBookByTitle(String title);
+	//public List<Pair<email, password>> findAuthorAndBookByTitle(String title);
 }
